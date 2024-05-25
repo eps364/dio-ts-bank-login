640aa327548c0a2d907f94698f9265e52340d3ca
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2024-05-25 08:44:52 -0300

    chore: Update email and password validation in login service

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,15 +1,25 @@
-import { login } from "./login"
+import { login } from "./login";
 
-describe('login', () => {
+describe("login", () => {
+  const mockEmail = "emerson@dio.bank";
+  const mockPassword = "123456";
+  it("Deve exibir um alert com boas vindas caso o email seja válido", async () => {
+    const response = await login(mockEmail, mockPassword);
+    expect(response).toBeTruthy();
+  });
 
-    const mockEmail = 'nath@dio.bank'
-    it('Deve exibir um alert com boas vindas caso o email seja válido', async() => {
-        const response = await login(mockEmail)
-        expect(response).toBeTruthy()
-    })
+  it("Deve exibir um erro caso o email seja inválido", async () => {
+    const response = await login("email@invalido.com", mockPassword);
+    expect(response).toBeFalsy();
+  });
 
-    it('Deve exibir um erro caso o email seja inválido', async() => {
-        const response = await login('email@invalido.com')
-        expect(response).toBeFalsy()
-    })
-})
+  it("Deve exibir um erro caso a senha seja inválida", async () => {
+    const response = await login(mockEmail, "123");
+    expect(response).toBeFalsy();
+  });
+
+  it("Deve exibir um alert com boas vindas caso a senha seja válida", async () => {
+    const response = await login(mockEmail, mockPassword);
+    expect(response).toBeTruthy();
+  });
+});
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,11 +1,18 @@
-import { api } from "../api"
+import { api } from "../api";
 
-export const login = async (email: string): Promise<boolean> => {
-    const data: any = await api
+export const login = async (
+  email: string,
+  passord: string
+): Promise<boolean> => {
+  const data: any = await api;
 
-    if(email !== data.email) {
-        return false
-    }
+  if (email !== data.email) {
+    return false;
+  }
 
-    return true
-}
+  if (passord !== data.password) {
+    return false;
+  }
+
+  return true;
+};
